68d165bf0cd4183bbccd378488531c9da280943e
Renat Chernov <renat3846@gmail.com>
2023-05-26 14:57:44 +0300

    add calendar

--- src/components/calendar/CalendarView.js
+++ src/components/calendar/CalendarView.js
@@ -1,29 +1,129 @@
-import * as React from "react";
-import Paper from "@material-ui/core/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import {
-  Scheduler,
-  MonthView,
-  Toolbar,
-  DateNavigator,
-  Appointments,
-  TodayButton,
-} from "@devexpress/dx-react-scheduler-material-ui";
-import { appointments } from "./appointments/appointments";
+// import * as React from 'react';
+// import Paper from '@material-ui/core/Paper';
+// import { ViewState } from '@devexpress/dx-react-scheduler';
+// import {
+//   Scheduler,
+//   MonthView,
+//   Toolbar,
+//   DateNavigator,
+//   Appointments,
+//   TodayButton,
+// } from '@devexpress/dx-react-scheduler-material-ui';
+// import { appointments } from './appointments/appointments';
 
-export default function Demo() {
-  const currentDate = new Date();
+// export default function Demo() {
+//   const currentDate = new Date();
 
-  return (
-    <Paper>
-      <Scheduler data={appointments}>
-        <ViewState defaultCurrentDate={currentDate} />
-        <MonthView />
-        <Toolbar />
-        <DateNavigator />
-        <TodayButton />
-        <Appointments />
-      </Scheduler>
-    </Paper>
-  );
-}
+//   return (
+//     <Paper>
+//       <Scheduler data={appointments}>
+//         <ViewState defaultCurrentDate={currentDate} />
+//         <MonthView />
+//         <Toolbar />
+//         <DateNavigator />
+//         <TodayButton />
+//         <Appointments />
+//       </Scheduler>
+//     </Paper>
+//   );
+// }
+
+import { Badge, Calendar } from 'antd';
+const getListData = (value) => {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event.',
+        },
+        {
+          type: 'success',
+          content: 'This is usual event.',
+        },
+      ];
+      break;
+    case 10:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event.',
+        },
+        {
+          type: 'success',
+          content: 'This is usual event.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event.',
+        },
+      ];
+      break;
+    case 15:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event',
+        },
+        {
+          type: 'success',
+          content: 'This is very long usual event。。....',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 1.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 2.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 3.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 4.',
+        },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+};
+const getMonthData = (value) => {
+  if (value.month() === 8) {
+    return 1394;
+  }
+};
+const Demo = () => {
+  const monthCellRender = (value) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  const cellRender = (current, info) => {
+    if (info.type === 'date') return dateCellRender(current);
+    if (info.type === 'month') return monthCellRender(current);
+    return info.originNode;
+  };
+  return <Calendar cellRender={cellRender} />;
+};
+export default Demo;
--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,21 @@ import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
 const CourseStatus = {
-  "PUBLISHED": "PUBLISHED",
-  "DRAFT": "DRAFT"
+  PUBLISHED: 'PUBLISHED',
+  DRAFT: 'DRAFT',
 };
 
-const { AssignmentModel, SyllabusModel, CommentModel, User, CourseUser, Course, Lesson, AnnouncementsModel, PostModel } = initSchema(schema);
+const {
+  AssignmentModel,
+  SyllabusModel,
+  CommentModel,
+  User,
+  CourseUser,
+  Course,
+  Lesson,
+  AnnouncementsModel,
+  PostModel,
+} = initSchema(schema);
 
 export {
   AssignmentModel,
@@ -19,5 +29,5 @@ export {
   Lesson,
   AnnouncementsModel,
   PostModel,
-  CourseStatus
+  CourseStatus,
 };
